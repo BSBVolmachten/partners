94a8595bd1eaea466787331e76a2512f643ae7ce
Frenk Smeets <Dah@live.nl>
2019-05-08 20:13:51 +0200

    Add files via upload

---
+++ updates/builder_table_create_bsbvolmachten_partners_main.php
@@ -0,0 +1,24 @@
+<?php namespace BsbVolmachten\Partners\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateBsbvolmachtenPartnersMain extends Migration
+{
+    public function up()
+    {
+        Schema::create('bsbvolmachten_partners_main', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->string('name', 255);
+            $table->string('url', 255);
+            $table->integer('sort_order');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('bsbvolmachten_partners_main');
+    }
+}
